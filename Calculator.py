71b3bcca37120dc4453e6f2daa7404338d74a811
AryaUMohan <aryamohanarya28@gmail.com>
2024-06-26 17:59:33 +0530

    successs

--- Calculator.py
+++ Calculator.py
@@ -1,6 +1,6 @@
 def calculator():
 
-    print("------------------------------CALCULATOR---------------------------")
+    print("*********************  CALCULATOR ********************************")
 
     print("Select Operation: ")
 
@@ -8,9 +8,9 @@ def calculator():
 
     print("2.Subtract")
 
-    print("3.Multiply")
+    print("3.Multiplication")
 
-    print("4.Divide")
+    print("4.Division")
 
     operation = input("Enter Choice (1/2/3/4): ")
 
@@ -22,21 +22,21 @@ def calculator():
 
         if operation == '1':
 
-            print(f"The result is : {num1 + num2}")
+            print(f"The Sum is : {num1 + num2}")
 
         elif operation == '2':
 
-            print(f"The result is : {num1 - num2}")
+            print(f"The Difference is : {num1 - num2}")
 
         elif operation == '3':
 
-            print(f"The result is : {num1 * num2}")
+            print(f"The Product is : {num1 * num2}")
 
         elif operation == '4':
 
             if num2 != 0:
 
-                print(f"The result is : {num1 / num2}")
+                print(f"The Quotient is : {num1 / num2}")
 
             else:
 
